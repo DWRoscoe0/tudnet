19b4c9fae5cab6f57dc77af4556a9fafb125cf2c
DWRoscoer <DWRoscoe@Comcast.net>
2018-07-28 06:04:28 -0400

    Replaced use of Properties class with TreeMap and did manual io.

--- DagBrowser7/src/allClasses/Persistent.java
+++ DagBrowser7/src/allClasses/Persistent.java
@@ -6,16 +6,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Enumeration;
-//// import java.util.Map;
-import java.util.Properties;
-import java.util.TreeSet;
+import java.util.TreeMap;
+import java.util.Iterator;
 
 public class Persistent 
 
 	/* This class implements persistent data for an app.
-	  It is implemented with a Properties file.
+	  It is implemented with a text file.
 	  It provides additional support for hierarchical properties
 	  using property path names whose components are separated by "/".
 	  So "key" is synonymous with "full path".
@@ -30,69 +27,63 @@ public class Persistent
 		private final String theFileString= "PersistentData.txt";
 		
 		// Internal state.
-		private Properties theProperties= null;
+		private TreeMap<Object,Object> theMap= null;
+		  // TreeMap provides alphabetically sorted output to text file.
   	private FileOutputStream theFileOutputStream = null;
 		
 	  public void initializeV()
-	    /* This method loads the configuration from external files.
+	    /* This method loads the persistent data from an external file.
 	      It is possible to eliminate this method, and triggered by lazy loading,
-	      triggered by the first call that needs theProperties variable defined.
+	      triggered by the first call that needs theMap variable defined.
 	      But because finalizeV() must be called to write any changes,
 	      we might as well just call initializeV() as well.
 	      */
 	    {
-	  	  loadPropertiesV( theFileString );
+	  	  loadDataV( theFileString );
 	  	  
-	  		///dbg storePairV( "kayX", "valueX" );
-		  	///dbg storePairV( "kayY", "valueY" );
-		  	///dbg storePairV( "kayZ", "valueZ" );
+	  		///dbg storePairV( "keyX", "valueX" );
+		  	///dbg storePairV( "keyY", "valueY" );
+		  	///dbg storePairV( "keyZ", "valueZ" );
 	  	  
 	    	}
 		
 	  
 	  public void finalizeV()
-	  /* This method loads the configuration from external files.  */
+	  /* This method stores the persistent data to the external file.  */
 	  {
-		  storePropertiesV( theFileString );
+		  storeDataV( theFileString );
 	  	}
 	  
-	  private void loadPropertiesV( String fileString )
-	    /* This method creates a Properties instance
-	      from the contents of the external properties file
-	      whose name is fileString and stores it in theProperties field.  
+	  private void loadDataV( String fileString )
+	    /* This method creates a Map 
+	      from the contents of the external text file
+	      whose name is fileString and stores it in theMap field.  
 	      If no properties were read then the result will be empty.
 	      If some properties were read then the result will contain them.
 	      The result will never be null.
 	      */
 	    {
-	  		theProperties= // Adapt to write properties alphabetically.
-					new Properties() { 
-						@Override
-		  	    public synchronized Enumeration<Object> keys() {
-		  	        return Collections.enumeration(new TreeSet<Object>(super.keySet()));
-		  	    	}
-		  			};
+	  		theMap= new TreeMap<Object,Object>(); // Create empty Map. 
 
 	  		FileInputStream configFileInputStream= null;
 
 		  	try { 
 		  		  configFileInputStream = 
 		  		  		new FileInputStream(Config.makeRelativeToAppFolderFile( fileString ));
-				  	////theProperties.load(configFileInputStream);
 		  		  loadV(configFileInputStream);
 			  		} 
 			  	catch (FileNotFoundException theFileNotFoundException) { 
-			  		appLogger.warning("Config.loadPropertiesV(..)"+theFileNotFoundException);
+			  		appLogger.warning("Config.loadDataV(..)"+theFileNotFoundException);
 			  		}
 			  	catch (Exception theException) { 
-			  		appLogger.exception("Config.loadPropertiesV(..)", theException);
+			  		appLogger.exception("Config.loadDataV(..)", theException);
 			  		}
 			  	finally { 
 			  		try { 
 			  			if ( configFileInputStream != null ) configFileInputStream.close(); 
 			  			}
 				  	catch (Exception theException) { 
-				  		appLogger.exception("Config.loadPropertiesV(..)", theException);
+				  		appLogger.exception("Config.loadDataV(..)", theException);
 				  		}
 			  		}
 	    	}
@@ -100,13 +91,12 @@ public class Persistent
 	  private void loadV( FileInputStream configFileInputStream )
 	    throws IOException
 	    {
-		  	//// theProperties.load(configFileInputStream);
 	  	  byte[] lineAB= new byte[1024];
 	  	  int CI= configFileInputStream.read();
 		  	while (true) { // Process all lines.
 		  		if (CI==-1) break; // Exit loop if end of file.
 		  		int offsetI= 0;
-		  	  while (true) // Read, convert, and store a line of characters.
+		  	  while (true) // Read, convert, and store a line in characters array.
 		  	  	{
 		  	  	  if  // Exit loop if end of line character, or end of file.
 		  	  	    ((CI=='\n') || (CI=='\r') || (CI==-1)) 
@@ -114,36 +104,38 @@ public class Persistent
 		  	  	  lineAB[offsetI++]= (byte)(0xff & CI); // Store converted byte.
 		  		  	CI= configFileInputStream.read(); // Read next byte.
 		  	  		}
-		  	  String lineString= // Convert to String.
+		  	  String lineString= // Convert char array to a String.
 		  	  		new String(lineAB, 0, offsetI);
-		  	  processLineV(lineString); // Process appropriately.
+		  	  processLineV(lineString); // Process string appropriately.
   		  	CI= configFileInputStream.read(); // Read next byte.
 		  		}
 	    	}
 
 	  private void processLineV(String lineString)
+	    /* Reads lineString and saves any key-value entry present.  */
 		  { 
 	  		goReturn: {
 	  		goFail: {
 		  	    if (lineString.isEmpty())  // Empty line. 
-		  	    	break goReturn; // Ignore and success.
+		  	    	break goReturn; // Ignore with success.
 		  	    if (lineString.charAt(0) == '#') // Comment line.
-		  	    	break goReturn; // Ignore and success.
+		  	    	break goReturn; // Ignore with success.
 		  	    int offsetOfEqualsI= lineString.indexOf('=', 0); // First '='.
 		  	    if ( offsetOfEqualsI < 1) // No '=' in line or it's at beginning
 		  	    	break goFail; // Fail.
+		  	    // All is well.  Extract key and value and save them.
 		  	    String keyString= lineString.substring(0, offsetOfEqualsI);
 		  	    String valueString= lineString.substring(offsetOfEqualsI+1);
-		  	    theProperties.put(keyString,valueString);
-		  	    break goReturn;
+		  	    theMap.put(keyString,valueString);
+		  	    break goReturn; // Return with success.
 		  	} // goFail:
 		  		appLogger.warning("processLineV(..) failed, line=\""+lineString+"\"");
 		  	} // goReturn:
 		  }
 	  
-	  private void storePropertiesV( String fileString )
-	    /* This method stores the configuration properties
-	      to the external properties file whose name is fileString.  
+	  private void storeDataV( String fileString )
+	    /* This method stores the Map data to 
+	      the external text file whose name is fileString.  
 	      */
 	    {
 		  	try { 
@@ -152,14 +144,14 @@ public class Persistent
 		  			storeV( theFileOutputStream );
 			  		} 
 			  	catch (Exception theException) { 
-			  		appLogger.exception("Config.storePropertiesV(..)", theException);
+			  		appLogger.exception("Config.storeDataV(..)", theException);
 			  		}
 			  	finally { 
 			  		try { 
 			  			if ( theFileOutputStream != null ) theFileOutputStream.close(); 
 			  			}
 			  		catch ( Exception theException ) { 
-				  		appLogger.exception("Config.storePropertiesV(..)", theException);
+				  		appLogger.exception("Config.storeDataV(..)", theException);
 				  		}
 			  		}
 	    	}
@@ -167,33 +159,18 @@ public class Persistent
 	  private void storeV( FileOutputStream theFileOutputStream)
 	    throws IOException
 		  {
-		  	/*  ////
-		  	theProperties.store(
-		  	  theFileOutputStream, 
-					null//// "---Infogora persistent data file---"
-					);
-				*/  ////
 	  		writingStringV("#---NEW Infogora persistent data file---\n");
-		  	for // Write all elements in the map. 
-		  	  (Enumeration<?> e = theProperties.keys(); 
-		  			e.hasMoreElements();) // Enumeration has been sorted.
+	  		Iterator<?> theIterator = theMap.keySet().iterator();
+	  		while (theIterator.hasNext())
 			  	{ // Write one element.
-	          String key = (String)e.nextElement();
-		  			//// writingStringV("key=" + key + "\n");
-	          String val = (String)theProperties.get(key);
-		  			//// writingStringV("val=" + val + "\n");
-	          //// key = saveConvert(key, true, escUnicode);
-	          /* No need to escape embedded and trailing spaces for value, hence
-	           * pass false to flag.
-	           */
-	          //// val = saveConvert(val, false, escUnicode);
-	          writingStringV(key + "=" + val + "\n"); // Write in a line.
-	          //// bw.newLine();
+	  				String keyString = (String)theIterator.next();
+	          String valueString = (String)theMap.get(keyString);
+	          writingStringV(keyString + "=" + valueString + "\n"); // Write line.
 			  		}
 		  	}
 
     public void writingStringV( String theString ) throws IOException
-      // This method writes theString but it doesn't force a flush().
+      // This method writes theString to the text file OutputStream.
       {
     		byte[] buf = theString.getBytes(); // Getting byte buffer from String
     		theFileOutputStream.write(buf); // Writing it to stream memory.
@@ -303,13 +280,16 @@ public class Persistent
 		    or defaultValueString if there is none.
 		   	*/
 		  {
-				return theProperties.getProperty( keyString, defaultValueString );
+	  	  String valueString= (String)theMap.get(keyString);
+	  	  if (valueString == null)
+	  	  	valueString= defaultValueString;
+	  	  return valueString;
 			  }
 	
 	  public void putV( String keyString, String valueString )
 		  // Stores valuesString as the value associated with keyStrig.
 		  {
-				theProperties.setProperty( keyString, valueString );
+	  		theMap.put( keyString, valueString );
 			  }
 		
 		}
