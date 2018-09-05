b270db8537814ecccf8d70d6a4fdc0c85e479001
DWRoscoer <DWRoscoe@Comcast.net>
2018-09-05 02:56:52 -0400

    Changed State colors to be light purple and light blue, plus cleanup.

--- DagBrowser7/src/allClasses/UIColor.java
+++ DagBrowser7/src/allClasses/UIColor.java
@@ -5,58 +5,63 @@ import java.awt.Component;
 
 public class UIColor {
 
-	// Non-selected item colors.
+	// Colors for non-selected cells.
 	
 	  // Non-selected foreground colors.
 			public static final Color normalForgroundColor= Color.BLACK;
 
 		// Non-selected background colors.
 			public static final Color activeColor= Color.YELLOW; // Active, non-State.
+			  // This is rarely seen, but is very noticeable when used.
 
-  	// Non-selected background state colors, used in StateList class.
-			public static final Color activeStateColor= 
-					new Color(127,255,127); // LIGHT_GREEN, was Color.GREEN;
-			public static final Color inactiveStateColor= 
-					Color.PINK;
+  	// Non-selected background state colors used in StateList class.
+			public static final Color activeStateColor=
+					new Color(255,191,255); // light purple.
+			public static final Color inactiveStateColor=
+					new Color(191,191,255); // light blue.
 			
-	// Non-selected item colors.
-			
-		// Selection is done by swapping foreground and background colors.
+
+	// Colors for selected cells.
+
+		// Set by swapping foreground and background colors in setColorsV(.) method.
+
+
+  // Methods.
 
   public static void setColorsV
     ( Component theRenderComponent,
-      Color cachedJListBackgroundColor,
+      Color defaultBackgroundColor,
     	DataNode theDataNode,
       boolean isSelectedB,
       boolean hasFocusB
       )
     /* Sets appropriate foreground and background colors in theRenderComponent 
-      for displaying theDataNode, using the other parameters as context.
+      for displaying theDataNode, using the remaining parameters as context.
       */
     {
   		Color backgroundColor, foregroundColor;
-      setColors: { // Setting colors based on various conditions.
+      setColors: { 
+  			// Calculating colors based on various conditions.
     		{ // Set most probable colors.
       	  backgroundColor= // Set default background. 
-      	  		theDataNode.getBackgroundColor(cachedJListBackgroundColor);
+      	  		theDataNode.getBackgroundColor(defaultBackgroundColor);
       	        // This gets DataNode's preferred color, if it has one,
-      	  			// or the JList's background color if not.
+      	  			// or defaultBackgroundColor if not.
         	foregroundColor= // Set default foreground.
         			UIColor.normalForgroundColor;
     			}
         if ( ! hasFocusB ) break setColors; // Cell not focused, change nothing.
         if ( ! isSelectedB ) break setColors; // Same if cell not selected.
-        { // Cell is both selected and focused, so set special colors for that.
-        	// Swap the present background and foreground colors.
+        { // Cell is both selected and focused, so set special colors.
+        	// Do this by swapping the present background and foreground colors.
         	Color tmpColor= backgroundColor;
           backgroundColor= foregroundColor;
           foregroundColor= tmpColor; 
         	}
-        } // adjustColors:
-  		{ // We've decided on the colors to use, now set them.
-	    	theRenderComponent.setBackground( backgroundColor );
-	    	theRenderComponent.setForeground( foregroundColor );
-	  		}
-      }
+      } // setColors:
+	  		// We'decided on the colors to use, now set them in component.
+		    theRenderComponent.setBackground( backgroundColor );
+		    theRenderComponent.setForeground( foregroundColor );
+		  }
 
-}
+	}
