620a7dd9a625aada84c28cc21a415f421533048d
DWRoscoer <DWRoscoe@Comcast.net>
2018-12-16 05:38:38 -0500

    Make .bat script build the executable jar file.

--- DagBrowser7/launcher/BuildAndTest.bat
+++ DagBrowser7/launcher/BuildAndTest.bat
@@ -1,2 +1,8 @@
-rem  Simply run the executabe jar file.
+rem  Delete the old jar file.
+del Infogora.jar
+
+rem  Create new jar file Infogora.jar
+jar cvfm Infogora.jar Manifest.txt -C ../bin allClasses
+
+rem  Run the executabe jar file.
 Infogora.jar
