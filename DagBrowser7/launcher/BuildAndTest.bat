86e6f45e557ab4b74976ecb8cba44780ee7a2bb4
DWRoscoer <DWRoscoe@Comcast.net>
2018-12-28 04:28:01 -0500

    Use copied and .gitignored JRE to run jar file.

--- DagBrowser7/launcher/BuildAndTest.bat
+++ DagBrowser7/launcher/BuildAndTest.bat
@@ -4,5 +4,5 @@ del Infogora.jar
 rem  Create the new jar file.
 jar cvfm Infogora.jar Manifest.txt -C ../bin allClasses
 
-rem  Run the jar file.
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -jar Infogora.jar
+rem  Run the jar file using local copy of JRE.
+jre1.8.0_191\bin\java.exe -jar Infogora.jar
