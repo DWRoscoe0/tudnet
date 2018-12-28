552a8825b47921f878d8d8805de88caa0012f1b9
DWRoscoer <DWRoscoe@Comcast.net>
2018-12-28 03:42:06 -0500

    Explicitly reference installed JRE when running jar file.

--- DagBrowser7/launcher/BuildAndTest.bat
+++ DagBrowser7/launcher/BuildAndTest.bat
@@ -5,4 +5,4 @@ rem  Create the new jar file.
 jar cvfm Infogora.jar Manifest.txt -C ../bin allClasses
 
 rem  Run the jar file.
-Infogora.jar
+"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -jar Infogora.jar
