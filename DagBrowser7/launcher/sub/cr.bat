c45aef652a576dd10114ae955753bf4b54cc8e71
DWRoscoer <DWRoscoe@Comcast.net>
2019-01-04 01:37:44 -0500

    Changed "archive" to "test", and informational outputs.

--- DagBrowser7/launcher/sub/cr.bat
+++ DagBrowser7/launcher/sub/cr.bat
@@ -1,5 +1,5 @@
-del archive.7z
-del archive.exe
-..\7zr a archive.7z ..\7zr.exe -mx -mf=BCJ2
-copy /b ..\7zSD.sfx + config.txt + archive.7z archive.exe
+del test.7z
+del test.exe
+..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
+copy /b ..\7zSD.sfx + config.txt + test.7z test.exe
 
