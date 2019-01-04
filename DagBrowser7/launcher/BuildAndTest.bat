ef366f685136066f6920ddf599ebfd5995e31a3a
DWRoscoer <DWRoscoe@Comcast.net>
2019-01-04 01:08:24 -0500

    Some attempts to simplify that had mixed results.

--- DagBrowser7/launcher/BuildAndTest.bat
+++ DagBrowser7/launcher/BuildAndTest.bat
@@ -1,10 +1,6 @@
-rem  Create the new executable jar file Infogora.jar after deleting old one.
-del Infogora.jar
-jar cvfm Infogora.jar Manifest.txt -C ../bin allClasses
-
-rem  Run the new jar file using local copy of JRE.
-rem  jre1.8.0_191\bin\java.exe -jar Infogora.jar
-
 rem  Create new 7zip archive after deleting old one.
-del app.7z
-"C:\Program Files (x86)\7-Zip\7z" a -t7z -r app.7z jre1.8.0_191 Infogora.jar
+del test.7z
+"C:\Program Files (x86)\7-Zip\7z" a -t7z -r test.7z setup.exe
+
+copy /b 7zSD.sfx+config.txt+test.7z test.exe
+
