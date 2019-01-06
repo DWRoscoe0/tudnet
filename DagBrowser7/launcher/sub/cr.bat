db496aee47ed2a336de0aa79dd04ac6f3e9cfed7
DWRoscoer <DWRoscoe@Comcast.net>
2019-01-06 00:40:10 -0500

    Commit so staging area is clear for separate launcher experiments.

--- DagBrowser7/launcher/sub/cr.bat
+++ DagBrowser7/launcher/sub/cr.bat
@@ -1,5 +1,6 @@
 del test.7z
 del test.exe
-..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
+rem ..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
+..\7zr a test.7z ..\7zr.exe
 copy /b ..\7zSD.sfx + config.txt + test.7z test.exe
 
