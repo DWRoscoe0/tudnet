ede1504485a4baac6d6f604680fdf156858e46a5
DWRoscoer <DWRoscoe@Comcast.net>
2019-01-08 15:12:35 -0500

    Got it to execute bat script with useful pauses.

--- DagBrowser7/launcher1805m/test.bat
+++ DagBrowser7/launcher1805m/test.bat
@@ -1,7 +1,9 @@
 del test.7z
 del test.exe
 rem ..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
-7zr a test.7z 7zr.exe -mx -mf=BCJ2
+rem 7zr a test.7z 7zr.exe -mx -mf=BCJ2
+7zr a test.7z subTest.bat -mx -mf=BCJ2
 copy /b 7zSD.sfx + config.txt + test.7z test.exe
-pause
+pause Before execution...
 test.exe
+pause After execution...
