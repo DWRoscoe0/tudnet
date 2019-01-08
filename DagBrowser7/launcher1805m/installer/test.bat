73ebbbe6b1e4af6d759ef2c8ca75f00ae4fdcb54
DWRoscoer <DWRoscoe@Comcast.net>
2019-01-08 15:00:50 -0500

    Eliminated use of installer\ sub-directory and deleted.

--- DagBrowser7/launcher1805m/installer/test.bat
+++
@@ -1,6 +0,0 @@
-del test.7z
-del test.exe
-..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
-copy /b ..\7zSD.sfx + config.txt + test.7z test.exe
-pause
-test.exe
---
+++ DagBrowser7/launcher1805m/test.bat
@@ -0,0 +1,7 @@
+del test.7z
+del test.exe
+rem ..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
+7zr a test.7z 7zr.exe -mx -mf=BCJ2
+copy /b 7zSD.sfx + config.txt + test.7z test.exe
+pause
+test.exe
