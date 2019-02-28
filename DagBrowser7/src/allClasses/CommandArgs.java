ef2e04c83d2bf8ed0ae9ce8bb32b83b5f134b8ff
DWRoscoer <DWRoscoe@Comcast.net>
2019-02-28 05:14:04 -0500

    Added documentation to CommandArgs class.

--- DagBrowser7/src/allClasses/CommandArgs.java
+++ DagBrowser7/src/allClasses/CommandArgs.java
@@ -15,29 +15,58 @@ public class CommandArgs {
     https://github.com/jjenkov/cli-args and described at
     http://tutorials.jenkov.com/java-howto/java-command-line-argument-parser.html
 
-    Note, switch strings must always included the '-' character.
+    Arguments consist of
+    * switches, which are Strings that begin with "-" 
+      optionally followed by a switch value which can be either
+      * nothing
+      * boolean
+      * long
+      * double
+      * String array
+    * targets, which are strings remaining after all switches are processed.
 
-    ///enh Presently it provides mostly ways to return 
-    target and switch states statically.
-    Later it might also provide ways to do this sequential.
-    There is one exception.  Switches that can values 
-    must be interrogated for their values
-    using a variation of the switchValue(..) method 
-    before String[] targets() is called to 
-    prevent switch values being returned as targets.
+    Processing of arguments normally goes proceeds as follows:
+    * Construct an instance of this object or 
+      call parse(argStrings) on an existing instance.
+    * Process all the switches and their values by 
+      * testing for their presence using 
+        one more calls to switchPresent(), and-or
+      * getting the values of switches using one more calls to:
+        * switchValue(), which returns a String
+        * switchLongValue()
+        * switchDoubleValue()
+        * switchValues(), which returns a String array
+        * switchPojo(), which returns a pojo whose fields match the switches
+    * Call targets() to return all remaining arguments, 
+      arguments that are targets because they
+      were not processed earlier as switches or switch values. 
+
+    ///enh Presently only the presence of switches is significant.
+      Their order is not.  It might be worthwhile to provides 
+      a way to process arguments sequentially,
+      a way in which the switches are significant.
 
    */
 
   private String[] args = null;
 
-  private HashMap<String, Integer> switchIndexes = new HashMap<String, Integer>();
-  private TreeSet<Integer>         takenIndexes  = new TreeSet<Integer>();
+  private HashMap<String, Integer> switchIndexes= 
+      new HashMap<String, Integer>(); // Found switches and their positions.
+  private TreeSet<Integer> takenIndexes= 
+      new TreeSet<Integer>(); // Positions of switches that have been gotten.
 
-  public CommandArgs(String[] args){
+  
+  public CommandArgs(String[] args){ // Constructor.
       parse(args);
   }
 
-  public void parse(String[] arguments){
+  public void parse(String[] arguments)
+    /* This method parses the arguments list, defining the values of
+      switchIndexes and takenIndexes appropriately.
+      After this is done, the interrogation methods which follow
+      may be called.  
+      */
+    {
       this.args = arguments;
       //locate switches.
       switchIndexes.clear();
@@ -53,23 +82,35 @@ public class CommandArgs {
       }
   }
 
-  public String[] args() {
-      return args;
-  }
+  
+  // Interrogation methods follow.
+  
+  public String[] args() 
+    // Returns argument String array associated with this object.
+    { return args; }
 
-  public String arg(int index){
-      return args[index];
-  }
+  public String arg(int indexI)
+    // Returns the argument String whose index is indexI.
+    { return args[indexI]; }
 
-  public boolean switchPresent(String switchName) {
-      return switchIndexes.containsKey(switchName);
-  }
+  public boolean switchPresent(String switchName)
+    // Returns true if the switch switchName was parsed, false otherwise.
+    { return switchIndexes.containsKey(switchName); }
 
-  public String switchValue(String switchName) {
-      return switchValue(switchName, null);
-  }
+  public String switchValue(String switchName) 
+    /* Returns the String value of switchName.
+      If that switch did not appear in the parse arguments
+      then null is returned.
+      */
+    { return switchValue(switchName, null); }
 
-  public String switchValue(String switchName, String defaultValue) {
+  public String switchValue(String switchName, String defaultValue) 
+    /* Returns the String value associated with switchName, which is 
+      the String argument that followed the switch in the argument list.
+      If that switch did not appear in the parsed arguments or it had no value
+      then defaultValue is returned.
+      */
+    {
       if(!switchIndexes.containsKey(switchName)) return defaultValue;
 
       int switchIndex = switchIndexes.get(switchName);
@@ -80,120 +121,148 @@ public class CommandArgs {
       return defaultValue;
   }
 
-  public Long switchLongValue(String switchName) {
-      return switchLongValue(switchName, null);
-  }
+  public Long switchLongValue(String switchName)
+    /* Returns the Long value associated with switchName,
+      otherwise null. */
+    { return switchLongValue(switchName, null); }
 
-  public Long switchLongValue(String switchName, Long defaultValue) {
-      String switchValue = switchValue(switchName, null);
+  public Long switchLongValue(String switchName, Long defaultValue) 
+    /* Returns the Long value associated with switchName, 
+      otherwise defaultValue.  */
+    { 
+      String switchValue = switchValue(switchName, null); 
 
       if(switchValue == null) return defaultValue;
       return Long.parseLong(switchValue);
-  }
+      }
 
-  public Double switchDoubleValue(String switchName) {
+  public Double switchDoubleValue(String switchName)
+    /* Returns the Double value associated with switchName,
+      otherwise null. */
+    {
       return switchDoubleValue(switchName, null);
-  }
+      }
 
-  public Double switchDoubleValue(String switchName, Double defaultValue) {
+  public Double switchDoubleValue(String switchName, Double defaultValue) 
+    /* Returns the Double value associated with switchName, 
+      otherwise defaultValue. */
+    {
       String switchValue = switchValue(switchName, null);
 
       if(switchValue == null) return defaultValue;
       return Double.parseDouble(switchValue);
-  }
-
-
-  public String[] switchValues(String switchName) {
-      if(!switchIndexes.containsKey(switchName)) return new String[0];
-
-      int switchIndex = switchIndexes.get(switchName);
-
-      int nextArgIndex = switchIndex + 1;
-      while(nextArgIndex < args.length && !args[nextArgIndex].startsWith("-")){
-          takenIndexes.add(nextArgIndex);
-          nextArgIndex++;
       }
 
-      String[] values = new String[nextArgIndex - switchIndex - 1];
-      for(int j=0; j < values.length; j++){
+
+  public String[] switchValues(String switchName)
+    /* Returns a String array containing all the String argument values 
+      which follow the switch whose name is switchName.
+      If there are none then it returns an empty String array.
+      */
+    {
+      if // Return an empty array if the switch did not appear at all.
+        (!switchIndexes.containsKey(switchName)) return new String[0];
+
+      int switchIndex= switchIndexes.get(switchName); // Get index of switch.
+
+      int nextArgIndex = switchIndex + 1; // Get index of first switch value.
+      while // Take all the switch's arguments.
+        (nextArgIndex < args.length && !args[nextArgIndex].startsWith("-"))
+        {
+          takenIndexes.add(nextArgIndex); // Take the argument.
+          nextArgIndex++; // Advance argument index.
+          }
+
+      String[] values= // Allocate a sufficiently large argument result array.
+          new String[nextArgIndex - switchIndex - 1];
+      for(int j=0; j < values.length; j++){ // Copy arguments to result array.
           values[j] = args[switchIndex + j + 1];
       }
       return values;
   }
 
   public <T> T switchPojo(Class<T> pojoClass)
-    /* This method tries to create an object of type pojoClass and
-      load all its fields with values from the switch values whose switch names
-      match the field names in the pojoClass object,
-      matching except for replacing '_' with '-' and prepending a '-'.
-      If a switch is not present, 
-      the default value in field in the pojoClass object is not overridden.
-      This method eliminates the need for the app 
-      to test for individual switches and read their values.
-     */
+    /* This method returns an object of type T.
+      First it creates a new object of type T with its default values.
+      If there are any switches which match fields in the T object,
+      then it replaces the field's default value by the switch's value. 
+      When doing the matching, T field names have 
+      '_' replaced with '-' and have a '-' prepended,
+      because Java field names can not contain "-".
+      Switches can be gotten using one or more calls to this method 
+      instead of or in addition to methods which get individual switches.
+      */
     {
       try {
-          T pojo = pojoClass.newInstance();
+        T pojo= pojoClass.newInstance(); // Create T pojo with default values.
 
-          Field[] fields = pojoClass.getFields();
-          for(Field field : fields) {
-              Class<?> fieldType = field.getType(); /// added <?> to Class.
-              String fieldName = "-" + field.getName().replace('_', '-');
+        Field[] fields= pojoClass.getFields(); // Get pojo fields.
+        for(Field field : fields) { // Iterate over all pojo fields.
+          Class<?> fieldType= field.getType(); ///// I added <?> to Class.
+          String fieldName= // Calculate switch associated with field. 
+              "-" + field.getName().replace('_', '-');
 
-              if(fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)){
-                  field.set(pojo, switchPresent(fieldName) );
-              } else if(fieldType.equals(String.class)){
-                  if(switchValue(fieldName) != null){
-                      field.set(pojo, switchValue(fieldName ) );
-                  }
-              } else if(fieldType.equals(Long.class)    || fieldType.equals(long.class) ){
-                  if(switchLongValue(fieldName) != null){
-                      field.set(pojo, switchLongValue(fieldName) );
-                  }
-              } else if(fieldType.equals(Integer.class)    || fieldType.equals(int.class) ){
-                  if(switchLongValue(fieldName) != null){
-                      field.set(pojo, switchLongValue(fieldName).intValue() );
-                  }
-              } else if(fieldType.equals(Short.class)    || fieldType.equals(short.class) ){
-                  if(switchLongValue(fieldName) != null){
-                      field.set(pojo, switchLongValue(fieldName).shortValue() );
-                  }
-              } else if(fieldType.equals(Byte.class)    || fieldType.equals(byte.class) ){
-                  if(switchLongValue(fieldName) != null){
-                      field.set(pojo, switchLongValue(fieldName).byteValue() );
-                  }
-              } else if(fieldType.equals(Double.class)  || fieldType.equals(double.class)) {
-                  if(switchDoubleValue(fieldName) != null){
-                      field.set(pojo, switchDoubleValue(fieldName) );
-                  }
-              } else if(fieldType.equals(Float.class)  || fieldType.equals(float.class)) {
-                  if(switchDoubleValue(fieldName) != null){
-                      field.set(pojo, switchDoubleValue(fieldName).floatValue() );
-                  }
-              } else if(fieldType.equals(String[].class)){
-                  String[] values = switchValues(fieldName);
-                  if(values.length != 0){
-                      field.set(pojo, values);
-                  }
+          // Decode individual field into appropriate switch and copy value.
+          if(fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)){
+              field.set(pojo, switchPresent(fieldName) );
+          } else if(fieldType.equals(String.class)){
+              if(switchValue(fieldName) != null){
+                  field.set(pojo, switchValue(fieldName ) );
+              }
+          } else if(fieldType.equals(Long.class) || fieldType.equals(long.class) ){
+              if(switchLongValue(fieldName) != null){
+                  field.set(pojo, switchLongValue(fieldName) );
+              }
+          } else if(fieldType.equals(Integer.class) || fieldType.equals(int.class) ){
+              if(switchLongValue(fieldName) != null){
+                  field.set(pojo, switchLongValue(fieldName).intValue() );
+              }
+          } else if(fieldType.equals(Short.class) || fieldType.equals(short.class) ){
+              if(switchLongValue(fieldName) != null){
+                  field.set(pojo, switchLongValue(fieldName).shortValue() );
+              }
+          } else if(fieldType.equals(Byte.class) || fieldType.equals(byte.class) ){
+              if(switchLongValue(fieldName) != null){
+                  field.set(pojo, switchLongValue(fieldName).byteValue() );
+              }
+          } else if(fieldType.equals(Double.class) || fieldType.equals(double.class)) {
+              if(switchDoubleValue(fieldName) != null){
+                  field.set(pojo, switchDoubleValue(fieldName) );
+              }
+          } else if(fieldType.equals(Float.class) || fieldType.equals(float.class)) {
+              if(switchDoubleValue(fieldName) != null){
+                  field.set(pojo, switchDoubleValue(fieldName).floatValue() );
+              }
+          } else if(fieldType.equals(String[].class)){
+              String[] values = switchValues(fieldName);
+              if(values.length != 0){
+                  field.set(pojo, values);
               }
           }
+        }
 
-          return pojo;
-      } catch (Exception e) {
-          throw new RuntimeException("Error creating switch POJO", e);
-      }
+        return pojo;
+    } catch (Exception e) {
+        throw new RuntimeException("Error creating switch POJO", e);
+    }
   }
 
-  public String[] targets() {
-      String[] targetArray = new String[args.length - takenIndexes.size()];
+  public String[] targets()
+    /* This method returns an array of Strings containing
+      all the arguments that have not been taken, as either
+      * a switch which begins with a "-", or
+      * a switch value, one or more of which follows a switch.
+     */
+    {
+      String[] targetStrings= new String[args.length - takenIndexes.size()];
       int targetIndex = 0;
       for(int i = 0; i < args.length ; i++) {
           if( !takenIndexes.contains(i) ) {
-              targetArray[targetIndex++] = args[i];
+              targetStrings[targetIndex++] = args[i];
           }
       }
 
-      return targetArray;
-  }
+      return targetStrings;
+    }
 
-}
+  }
