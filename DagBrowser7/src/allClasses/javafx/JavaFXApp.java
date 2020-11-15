99f025ea8db7fd704ab0962ea9919326c783c94c
David W. Roscoe <DWRoscoe@Comcast.net>
2020-11-15 06:06:02 -0500

    Added some demo JavaFX controls and did clean up.

--- DagBrowser7/src/allClasses/javafx/JavaFXApp.java
+++ DagBrowser7/src/allClasses/javafx/JavaFXApp.java
@@ -1,18 +1,36 @@
 package allClasses.javafx;
 
+//// import com.gluonapplication.Icon;
+
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXApp extends Application {
   @Override
   public void start(Stage primaryStage) {
       try {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root,400,400);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-        primaryStage.setScene(scene);
+        BorderPane theBorderPane = new BorderPane();
+        Scene theScene = new Scene(theBorderPane,400,400);
+        theScene.getStylesheets().add(getClass()
+            .getResource("application.css").toExternalForm());
+        Label theLabel = 
+            new Label("JavaFX sub-Application window!");
+
+        Button theButton = new Button("Who wrote this app?");
+        theButton.setOnAction(e -> theLabel.setText(
+            "David Roscoe wrote this app!"));
+        
+        VBox theVBox = new VBox(15.0, theLabel, theButton);
+        theVBox.setAlignment(Pos.CENTER);
+        
+        theBorderPane.setCenter(theVBox);
+        primaryStage.setScene(theScene);
         primaryStage.show();
       } catch(Exception e) {
         e.printStackTrace();
--- DagBrowser7/src/allClasses/javafx/Main.java
+++ DagBrowser7/src/allClasses/javafx/Main.java
@@ -3,23 +3,32 @@ package allClasses.javafx;
 
 public class Main extends Object 
 
-  /* This class creates a thread with which to launch
-   * a JavaFX sub-Application.
-   * It could do other stuff, before of after,
-   * but it doesn't do that here.
+  /* This class is used to launch 
+   * the JavaFX part of an application.
    */
   {
     
-    public static void main(String[] args) {
-      new Thread(
-        new Runnable() {
-          @Override
-          public void run() {
-            JavaFXApp.main(args);
-            }
-          },
-        "JavaFXLauncher" // Thread name.
-        ).start();
-      }
+    public static void main(String[] args) 
+    
+      /* This method creates and starts a thread 
+       * which launches the JavaFX sub-Application.
+       * It queues this job on the JavaFX runtime queue,
+       * then it returns.
+       */
+      {
+        Runnable javaFXRunnable= // Create launcher Runnable. 
+          new Runnable() {
+            @Override
+            public void run() {
+              JavaFXApp.main(args); // Runs launcher.
+              }
+            };
+        Thread javaFXLauncherThread= // Create launcher thread.
+          new Thread(
+            javaFXRunnable,
+            "JavaFXLauncher" // Thread name.
+            );
+        javaFXLauncherThread.start(); // Start launcher thread.
+        }
 
     }
