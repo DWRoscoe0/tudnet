4e2f7ea9783422c3bc05968d44f37638b9d2cfc1
David W. Roscoe <DWRoscoe@Comcast.net>
2021-01-10 06:47:26 -0500

    Partially debugged default selection history purging.

--- DagBrowser7/src/allClasses/javafx/TreeStuff.java
+++ DagBrowser7/src/allClasses/javafx/TreeStuff.java
@@ -154,10 +154,10 @@ public class TreeStuff
                 theDataRoot // and this.
               ); 
           }
-        //// purgeAndTestB(
-        ////     selectionHistoryMapEpiNode,
-        ////     theDataRoot.getRootDataNode()
-        ////     );
+        purgeAndTestB(
+            selectionHistoryMapEpiNode,
+            theDataRoot.getParentOfRootDataNode()
+            );
         return resultTreeStuff;
         }
 
@@ -241,58 +241,63 @@ public class TreeStuff
         This method is recursively called on all of the child map entries.
         It removes child entries that
         * had true returned from the recursive call, and
-        * whose DataNode was not child 0, the first child,
-          which is the child used for default selection. 
-        It can not and does not remove the map entry containing theMapEpiNode.
-        That may be done by the caller if this method returns true.
+        * whose DataNode was NOT child 0, the first child,
+          which is the child used for default selection, and
+        * whose map entry is not the last one in the map,
+          which means it is the most recent selection.
+        It treats as purge successes data that is out of spec,
+        assuming that such data is best deleted at a higher level.
+
+        This method can not and does not remove 
+        the map entry containing theMapEpiNode.
+        That MAY be done by the caller if this method returns true.
 
         This method returns true if none of the children remain.
         It returns false otherwise, meaning that 
-          at least one child and possibly some if its descendants survived.
+        at least one child and possibly some if its descendants survived.
         */
       {
-          boolean allPurgedB= true; // Assume no child entries will survive.
+          boolean allPurgedB= true; // Default result for all children purged.
         toReturn: {
-          if (null == theMapEpiNode) // If input map is null
-            break toReturn; // exit now treating it as a purge success.
+          if (null == theMapEpiNode) break toReturn; // No map child entries.
           Set<Map.Entry<EpiNode,EpiNode>> theSetOfMapEntrys= 
               theMapEpiNode.getLinkedHashMap().entrySet();
           List<Map.Entry<EpiNode,EpiNode>> theListOfMapEntrys= 
               new ArrayList<Map.Entry<EpiNode,EpiNode>>(theSetOfMapEntrys);
                 // Copy to avoid ConcurrentModificationException.
+          int lastEntryIndexI= theListOfMapEntrys.size()-1;
           Iterator<Map.Entry<EpiNode,EpiNode>> entryIterator= 
-              //// theSetOfMapEntrys.iterator();
               theListOfMapEntrys.iterator();
         childLoop: while(true) { // Iterate through child entries.
-          if (! entryIterator.hasNext()) // Any more entries? 
-            break childLoop; // No, so exit loop.
-        oneChild: { // Process one child entry.
-          Map.Entry<EpiNode,EpiNode> childMapEntry= // Get entry.
-              entryIterator.next();
-          EpiNode valueEpiNode= childMapEntry.getValue();
-          if (null == valueEpiNode) // If value is null
-            break oneChild; // done with child which is considered purged.
+          if (! entryIterator.hasNext()) break childLoop; // No more children.
+        success: { failure: { 
+          Map.Entry<EpiNode,EpiNode> childMapEntry= entryIterator.next();
+          EpiNode valueEpiNode= childMapEntry.getValue(); // Try to get value.
+        removeAndSuccess: {
+          if (null == valueEpiNode) break success; // Success if no value.
           MapEpiNode valueMapEpiNode= valueEpiNode.tryMapEpiNode();
-          if (null == valueMapEpiNode) // If it's not a Map value
-            break oneChild; // done with child which is considered purged.
-          String keyString= childMapEntry.getValue().toString();
-          //// int childIndexI= theDataNode.getIndexOfNamedChild(keyString);
+          if (null == valueMapEpiNode) break success; // Success if not map.
+          String keyString= childMapEntry.getKey().toString();
           DataNode childDataNode= theDataNode.getNamedChildDataNode(keyString);
-        childPurgeFailCauses: {
-          boolean childPurgedB= // Try recursive child purge.
-              purgeAndTestB(valueMapEpiNode,childDataNode); 
-          if (! childPurgedB) // The child was not completely purge-able
-            break childPurgeFailCauses; // Done with this child entry.
-          ////// Here should go the child=0 test.
-          theMapEpiNode.removeEpiNode( // Remove purge-able child entry
-              childMapEntry.getKey().toString()); // by it's name.
-          break oneChild; // Done with this child entry.
-        } // childPurgeFailCauses:
+          if (null == childDataNode) break success; // Success if name failed.
+          boolean childPurgeAbleB= // Try recursive child purge and purge test.
+            purgeAndTestB(valueMapEpiNode,childDataNode); 
+          if (! childPurgeAbleB) break failure; // Failure if test failed.
+          if (0 == theDataNode.getIndexOfNamedChild(keyString)) // If is child 0
+            break removeAndSuccess; // remove with success.
+          if (theListOfMapEntrys.get(lastEntryIndexI) != childMapEntry)
+            break removeAndSuccess; // Remove with success if not last entry.
+          break failure; // so purge failure.
+        } // removeAndSuccess:
+          theMapEpiNode.removeEpiNode( // Remove purge-able entry from map
+              childMapEntry.getKey().toString()); // by name.
+          break success; // Tests passed.  Child removed.  Treat as success.
+        } // failure: Something caused child purge to fail.
           allPurgedB= false; // so entire parent map fails purge also.
-        } // oneChild:
-        } // childLoop:
-        } // toReturn.
-          return allPurgedB; // Return whether all children were purged.
+        } // success: This child is done.
+        } // childLoop: Continue with next child entry.
+        } // toReturn: We're done.
+          return allPurgedB; // Return whether purge of all children succeeded.
         }
 
     public DataNode getParentDataNode()
