023e778eda34c3210baf0ab89275c203729fdf11
David W. Roscoe <DWRoscoe@Comcast.net>
2021-11-11 18:17:02 -0500

    Move launcher1805m/ to memoryHole/.

--- DagBrowser7/launcher1805m/test.bat
+++
@@ -1,9 +0,0 @@
-del test.7z
-del test.exe
-rem ..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
-rem 7zr a test.7z 7zr.exe -mx -mf=BCJ2
-7zr a test.7z subTest.bat -mx -mf=BCJ2
-copy /b 7zSD.sfx + config.txt + test.7z test.exe
-pause Before execution...
-test.exe
-pause After execution...
