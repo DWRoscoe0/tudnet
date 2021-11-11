71ce0c8054333a3bcf443d4a7270f613a6c04d14
David W. Roscoe <DWRoscoe@Comcast.net>
2021-11-11 18:04:57 -0500

    Move launcher/ to memoryHole/.

--- DagBrowser7/launcher/BuildAndTest.bat
+++
@@ -1,6 +0,0 @@
-rem  Create new 7zip archive after deleting old one.
-del test.7z
-"C:\Program Files (x86)\7-Zip\7z" a -t7z -r test.7z setup.exe
-
-copy /b 7zSD.sfx+config.txt+test.7z test.exe
-
--- DagBrowser7/launcher/sub/cr.bat
+++
@@ -1,6 +0,0 @@
-del test.7z
-del test.exe
-rem ..\7zr a test.7z ..\7zr.exe -mx -mf=BCJ2
-..\7zr a test.7z ..\7zr.exe
-copy /b ..\7zSD.sfx + config.txt + test.7z test.exe
-
