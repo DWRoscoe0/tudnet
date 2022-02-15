29e3957759b3f577e9fcbfcd46bfe62c3d7ab92b
David W. Roscoe <DWRoscoe@Comcast.net>
2022-02-15 00:46:52 -0500

    EpiTreeItem JavaFX clean-up.

--- app/src/allClasses/javafx/EpiTreeItem.java
+++ app/src/allClasses/javafx/EpiTreeItem.java
@@ -1,7 +1,6 @@
 package allClasses.javafx;
 
 import java.util.ArrayList;
-//// import java.util.Iterator;
 
 import allClasses.DataNode;
 import javafx.collections.ObservableList;
@@ -11,11 +10,20 @@ public class EpiTreeItem
 
   extends TreeItem<DataNode>
 
-  /* This class acts as a kind of bridge between JavaFX TreeItems
-   * and DataNodes.
-   * It lazily creates child TreeItems, one for each child DataNode,
-   * meaning only when the TreeItem children are needed, not before.
+  /* The EpiTreeItems class acts as a bridge between 
+   * the JavaFX user interface and DataNodes.
+   * each instance contains a reference to a value DataNode and
+   * a list of child EpiTreeItems.
    * 
+   * The reference to the value DataNode does not change,
+   * but the DataNode content might not be fully evaluated at first.
+   * And after being evaluated, the content of the DataNode might change.
+   * 
+   * The reference to the list of child EpiTreeItems does not change.
+   * The contents of the list is lazily evaluated, triggered when it is gotten.
+   * Before evaluation the list is empty.
+   * After evaluation it might or might not be empty.
+   *
    * ///org I was going to try to make this class generic,
    * and remove the dependency on the DataNode class,
    * but some tree node functionality is needed,
@@ -29,7 +37,7 @@ public class EpiTreeItem
 
     public EpiTreeItem(DataNode theDataNode) // Constructor. 
       {
-        super(theDataNode); // Was setValue(theDataNode);
+        super(theDataNode); // Set the value.
         }
 
     @Override 
@@ -43,45 +51,44 @@ public class EpiTreeItem
     public ObservableList<TreeItem<DataNode>> getChildren()
       /* This method returns the TreeItem's child list.
        * It first checks to see whether the TreeItem child list 
-       * has been calculated based on the child list in the value DataNode.
-       * If not, then it calculates and stores it.
-       * Finally it returns the TreeItem's child list.
+       * has been evaluated based on the child list in the value DataNode.
+       * If not, then it evaluates and stores it.
+       * Finally it returns list.
        * 
-       * Note-1: childCacheLoadedB is set before loading is complete!
-       * This is done because super.getChildren() called getChildren(),
-       * specifically in TreeItem.updateExpandedDescendentCount(boolean reset), 
-       * which resulted in a StackOverflowError when 
-       * childCacheLoadedB was set after loading was complete.
+       * Note-1: For a while, childCacheLoadedB was set 
+       *   before loading is complete!
+       *   This was done because super.getChildren() called getChildren(),
+       *   specifically in 
+       *     TreeItem.updateExpandedDescendentCount(boolean reset), 
+       *   which resulted in a StackOverflowError when 
+       *   childCacheLoadedB was set after loading was complete.
+       *   Later this was no longer true, so that assignment was moved down.
        * 
-       * Note-2: This method originally lazily-evaluated children 
-       * and added them to the TreeItem.getChildren() ObservableList 
-       * one-at-a-time.
-       * This resulted in an unwanted selection when a node was expanded
-       * if the selection was below the expanded node.
-       * This problem disappeared when the entire collection of children
-       * was evaluated and added at once, similar to
-       * the way it is done in the TreeItem documentation example of
-       * an overridden getChildren() that uses lazy-evaluation. 
+       * Note-2: This method originally lazily-evaluated individual children 
+       *   and added them to the TreeItem.getChildren() ObservableList 
+       *   one-at-a-time.
+       *   This resulted in an unwanted selection when a node was expanded
+       *   if the selection was below the expanded node.
+       *   This problem disappeared when the entire collection of children
+       *   was evaluated and added at once, similar to
+       *   the way it is done in the TreeItem documentation example of
+       *   an overridden getChildren() that uses lazy-evaluation.
+       *   ///opt Figure out a way of evaluating children one at a time. 
        */
       {
         if (! childCacheLoadedB) // Load cache if not loaded yet.
           { // Load cache.
-            childCacheLoadedB= true; // See Note-1 above.
+            // childCacheLoadedB= true; // Moved down.  See Note-1 above.
             DataNode parentDataNode= getValue();
             ArrayList<EpiTreeItem> childrenListOfTreeItems= // Make empty list. 
                 new ArrayList<EpiTreeItem>();
-            /*  ////
-            Iterator<DataNode> theIterator=
-                parentDataNode.getChildListOfDataNodes().iterator();
-            while (theIterator.hasNext()) { // See Note-2 above.
-              DataNode childDataNode= theIterator.next();
-              accumulatorList.add(new EpiTreeItem(childDataNode));
-              }
-            */  ////
             for // Add all children as TreeItems.  See Note-2 above.
-              (DataNode childDataNode : parentDataNode.getChildrenAsListOfDataNodes())
+              ( DataNode childDataNode : 
+                parentDataNode.getChildrenAsListOfDataNodes() 
+                )
               childrenListOfTreeItems.add(new EpiTreeItem(childDataNode));
             super.getChildren().setAll(childrenListOfTreeItems);
+            childCacheLoadedB= true; // See Note-1 above.
             }
         return super.getChildren();
         }
