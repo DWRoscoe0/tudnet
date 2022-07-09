c4dcb93a3c6f06fe9c17add672ef87f75f0468c9
David W. Roscoe <DWRoscoe@Comcast.net>
2022-07-09 16:35:08 -0400

    Check for excessive time anomalies in OSTime operations.

--- app/src/allClasses/OSTime.java
+++ app/src/allClasses/OSTime.java
@@ -36,10 +36,18 @@ public class OSTime
         }
   
     static String nonZeroQuotientAsPerCentString(long dividendL,long divisorL)
+      /* 
+       * This method returns a String representing the result of
+       * dividing dividendL by divisorL.
+       * The value is represented as follows:
+       * * "" means exactly 0^
+       * * "00%" to "99%" and "100" for other values.
+       * 
+       */
       {
         String resultString;
         if (0 >= dividendL)
-          resultString= ""; // We don't bother showing if 0%.
+          resultString= ""; // We don't bother showing if exactly 0%.
           else 
           resultString= quotientAs3CharacterPercentString(dividendL,divisorL);
         return resultString;
@@ -47,6 +55,13 @@ public class OSTime
   
     static String quotientAs3CharacterPercentString(
         long dividendL,long divisorL)
+      /* 
+       * This method returns a String representing the result of
+       * dividing dividendL by divisorL.
+       * The value is represented by the values
+       * "00%" to "99%" and "100".
+       * 
+       */
       {
         double perCentD= 100. * (double)dividendL / divisorL;
         String resultString;
@@ -59,12 +74,12 @@ public class OSTime
         return resultString;
         }
   
-    static DutyCycle directoryDutyCycle= new DutyCycle();
-    static DutyCycle writingDutyCycle= new DutyCycle();
-    static DutyCycle syncingDutyCycle= new DutyCycle();
-    static DutyCycle closingDutyCycle= new DutyCycle();
-    static DutyCycle readingDutyCycle= new DutyCycle();
-    static DutyCycle deletingDutyCycle= new DutyCycle();
+    static DutyCycle directoryDutyCycle= new DutyCycle("Directory-Read");
+    static DutyCycle writingDutyCycle= new DutyCycle("Data-Write");
+    static DutyCycle syncingDutyCycle= new DutyCycle("File-Sync");
+    static DutyCycle closingDutyCycle= new DutyCycle("Close");
+    static DutyCycle readingDutyCycle= new DutyCycle("Data-Read");
+    static DutyCycle deletingDutyCycle= new DutyCycle("File-Delete");
     static long osTotalTimeActiveMsL;
     static String reportAccumulatorString;
     static long lastReportTimeNsL;
@@ -74,10 +89,42 @@ public class OSTime
     static class DutyCycle
       /* This class is used to calculate the duty-cycle of operations.
        * It was created to do this for IO operations,
-       * but it could be used for anything.
+       * but it could be used for any operation with a beginning and an ending.
        * The code is based on the assumption of a single thread.
        */
       {
+        static void accumulateOSTimeReportV(
+          DutyCycle theDutyCycle, String labelString)
+        /* Calculates a String representing percentage activity
+         * for theDutyCycle using laabelString as the label,
+         * and appends it to reportAccumulatorString.
+         * It also resets for the next activity measurement.
+         */
+        {
+          String measurementString= 
+              theDutyCycle.resetAndGetOSString(thisReportTimeNsL);
+          if ("" != measurementString) // If got non-0 measurement,
+            measurementString= labelString+measurementString; // prepend label.
+          reportAccumulatorString+= measurementString;
+          }
+
+        public String resetAndGetOSString(long timeNowNsL)
+          {
+            updateActivityV( // Adjust appropriate accumulator for present time.
+                operationIsActiveB,timeNowNsL);
+            osTotalTimeActiveMsL+= timeActiveNsL;
+            long totalTimeSincePreviousReportNsL= 
+                timeActiveNsL + timeInactiveNsL;
+            String resultString= nonZeroQuotientAsPerCentString(
+                timeActiveNsL, totalTimeSincePreviousReportNsL);
+
+            // Reset both accumulators for next time period.
+            timeActiveNsL= 0;
+            timeInactiveNsL= 0;
+      
+            return resultString;
+            }
+
         public void updateActivityWithTrueV()
           {
             updateActivityV(true);
@@ -94,6 +141,11 @@ public class OSTime
             }
 
         public void updateActivityV(boolean newActivityB,long timeNowNsL)
+          /* This method updates variables based on 
+           * newActivity and the time timeNowNsL.
+           * This method should be called whenever activity has changed or
+           * an activity report is being generated.
+           */
           {
             long timeSinceLastActivityChangeNsL= 
                timeNowNsL - timeOfLastActivityChangeNsL;
@@ -103,37 +155,31 @@ public class OSTime
               timeInactiveNsL+= timeSinceLastActivityChangeNsL;
             operationIsActiveB= newActivityB;
             timeOfLastActivityChangeNsL= timeNowNsL;
+            doLimitCheckV();
             }
 
-        static void accumulateOSTimeReportV(
-          DutyCycle theDutyCycle, String labelString)
-        /* Returns string representing OS%, or "" if % is 0.
-         * It also resets for the next measurement.
-         */
-        {
-          String resultString= theDutyCycle.resetAndGetOSString(thisReportTimeNsL);
-          if ("" != resultString) // % not 0
-            resultString= labelString + resultString; // so append to label.
-          reportAccumulatorString+= resultString;
-          }
-
-        public String resetAndGetOSString(long timeNowNsL)
+        private void doLimitCheckV()
+          /* This method tests whether the activity maximum time limit
+           * has been exceeded.  If it has then it reports it.
+           * This method should be called often enough
+           * to give the user timely reports.
+           */
           {
-            updateActivityV( // Adjust appropriate accumulator for present time.
-                operationIsActiveB,timeNowNsL);
-            osTotalTimeActiveMsL+= timeActiveNsL;
-            long totalTimeSincePreviousReportNsL= 
-                timeActiveNsL + timeInactiveNsL;
-            String resultString= nonZeroQuotientAsPerCentString(
-                timeActiveNsL, totalTimeSincePreviousReportNsL);
-      
-            // Reset both accumulators for next time period.
-            timeActiveNsL= 0;
-            timeInactiveNsL= 0;
-      
-            return resultString;
+            Anomalies.testAndDisplayDialogReturnString(
+                nameString,
+                operationIsActiveB ? "ACTIVE" : "Complete", 
+                1500, // temporary 1.5 second limit on all operations.
+                timeActiveNsL/1000000 // convert ns to ms
+                );
             }
 
+        private DutyCycle(String nameString) // constructor.
+          {
+            super();
+            this.nameString= nameString;
+            }
+
+        private String nameString;
         private boolean operationIsActiveB= false;
         private long timeOfLastActivityChangeNsL;
         private long timeActiveNsL;
